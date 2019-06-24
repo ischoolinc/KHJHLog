5d5e5a4c36621bb4a8061ced43b400b5bed152d4
Win5109 <win5109@gmail.com>
2019-06-24 11:43:03 +0800

    2019/6/24 俊緯更新 更新UI

--- KHJHLog/frmClassOrder.Designer.cs
+++ KHJHLog/frmClassOrder.Designer.cs
@@ -28,12 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.grdClassOrder = new DevComponents.DotNetBar.Controls.DataGridViewX();
-            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
-            this.cmbSchool = new DevComponents.DotNetBar.Controls.ComboBoxEx();
-            this.labelX1 = new DevComponents.DotNetBar.LabelX();
-            this.btnExport = new DevComponents.DotNetBar.ButtonX();
             this.colSchool = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colClassName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colTeacherName = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -47,6 +43,11 @@
             this.colLockComment = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colGradeYear = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colDisplayOrder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.cmbSchool = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.btnExport = new DevComponents.DotNetBar.ButtonX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
             ((System.ComponentModel.ISupportInitialize)(this.grdClassOrder)).BeginInit();
             this.SuspendLayout();
             // 
@@ -73,14 +74,14 @@
             this.colLockComment,
             this.colGradeYear,
             this.colDisplayOrder});
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.grdClassOrder.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.grdClassOrder.DefaultCellStyle = dataGridViewCellStyle3;
             this.grdClassOrder.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
             this.grdClassOrder.Location = new System.Drawing.Point(12, 58);
             this.grdClassOrder.Name = "grdClassOrder";
@@ -88,64 +89,8 @@
             this.grdClassOrder.RowHeadersVisible = false;
             this.grdClassOrder.RowTemplate.Height = 24;
             this.grdClassOrder.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grdClassOrder.Size = new System.Drawing.Size(1234, 633);
+            this.grdClassOrder.Size = new System.Drawing.Size(1229, 623);
             this.grdClassOrder.TabIndex = 0;
-            // 
-            // buttonX1
-            // 
-            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.buttonX1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonX1.BackColor = System.Drawing.Color.Transparent;
-            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.buttonX1.Location = new System.Drawing.Point(1069, 18);
-            this.buttonX1.Name = "buttonX1";
-            this.buttonX1.Size = new System.Drawing.Size(75, 23);
-            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.buttonX1.TabIndex = 1;
-            this.buttonX1.Text = "查詢";
-            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
-            // 
-            // cmbSchool
-            // 
-            this.cmbSchool.DisplayMember = "Text";
-            this.cmbSchool.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.cmbSchool.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbSchool.FormattingEnabled = true;
-            this.cmbSchool.ItemHeight = 19;
-            this.cmbSchool.Location = new System.Drawing.Point(92, 16);
-            this.cmbSchool.Name = "cmbSchool";
-            this.cmbSchool.Size = new System.Drawing.Size(553, 25);
-            this.cmbSchool.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.cmbSchool.TabIndex = 2;
-            // 
-            // labelX1
-            // 
-            this.labelX1.AutoSize = true;
-            this.labelX1.BackColor = System.Drawing.Color.Transparent;
-            // 
-            // 
-            // 
-            this.labelX1.BackgroundStyle.Class = "";
-            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.labelX1.Location = new System.Drawing.Point(12, 16);
-            this.labelX1.Name = "labelX1";
-            this.labelX1.Size = new System.Drawing.Size(67, 20);
-            this.labelX1.TabIndex = 3;
-            this.labelX1.Text = "選擇學校：";
-            // 
-            // btnExport
-            // 
-            this.btnExport.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExport.BackColor = System.Drawing.Color.Transparent;
-            this.btnExport.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnExport.Location = new System.Drawing.Point(1167, 18);
-            this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(75, 23);
-            this.btnExport.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.btnExport.TabIndex = 4;
-            this.btnExport.Text = "匯出";
-            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
             // 
             // colSchool
             // 
@@ -235,11 +180,82 @@
             this.colDisplayOrder.ReadOnly = true;
             this.colDisplayOrder.Width = 90;
             // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonX1.BackColor = System.Drawing.Color.Transparent;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(1068, 17);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 1;
+            this.buttonX1.Text = "查詢";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // cmbSchool
+            // 
+            this.cmbSchool.DisplayMember = "Text";
+            this.cmbSchool.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbSchool.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSchool.FormattingEnabled = true;
+            this.cmbSchool.ItemHeight = 19;
+            this.cmbSchool.Location = new System.Drawing.Point(92, 16);
+            this.cmbSchool.Name = "cmbSchool";
+            this.cmbSchool.Size = new System.Drawing.Size(553, 25);
+            this.cmbSchool.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cmbSchool.TabIndex = 2;
+            // 
+            // labelX1
+            // 
+            this.labelX1.AutoSize = true;
+            this.labelX1.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.Class = "";
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(12, 18);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(67, 20);
+            this.labelX1.TabIndex = 3;
+            this.labelX1.Text = "選擇學校：";
+            // 
+            // btnExport
+            // 
+            this.btnExport.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.BackColor = System.Drawing.Color.Transparent;
+            this.btnExport.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnExport.Location = new System.Drawing.Point(1166, 17);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "匯出";
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // labelX2
+            // 
+            this.labelX2.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.Class = "";
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(651, 17);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(318, 23);
+            this.labelX2.TabIndex = 5;
+            this.labelX2.Text = "說明：「實際人數」包含「休學人數」及「輟學人數」";
+            // 
             // frmClassOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1250, 691);
+            this.Controls.Add(this.labelX2);
             this.Controls.Add(this.btnExport);
             this.Controls.Add(this.labelX1);
             this.Controls.Add(this.cmbSchool);
@@ -277,5 +293,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colLockComment;
         private System.Windows.Forms.DataGridViewTextBoxColumn colGradeYear;
         private System.Windows.Forms.DataGridViewTextBoxColumn colDisplayOrder;
+        private DevComponents.DotNetBar.LabelX labelX2;
     }
 }
